414f06fe979ec766505633bcfc48523b7d836d67
a2mar <a2mar.protonmail.ch>
2018-06-03 15:24:42 +0200

    integrated cards WITH flipping

--- app/src/main/java/com/a2mar/smartcards/PracticeActivity.java
+++ app/src/main/java/com/a2mar/smartcards/PracticeActivity.java
@@ -1,5 +1,7 @@
 package com.a2mar.smartcards;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,40 +9,48 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.a2mar.smartcards.animations.FlipCard;
+
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.function.ToDoubleBiFunction;
 
 public class PracticeActivity extends AppCompatActivity {
 
     private static ArrayList<VocCard> vocCardList = null;
+    private int count;
+
+
+    private View mCardFrontLayout;
+    private View mCardBackLayout;
+    private AnimatorSet mSetLeftOut;
+    private AnimatorSet mSetRightIn;
+    private AnimatorSet mSetRightOut;
+    private AnimatorSet mSetLeftIn;
+    private boolean isBackRevealed = false;
+    private boolean isBackVisible = true;
+    private int angle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
 
-        // Get the Intent that started this activity and extract the string
+        // Get the Intent that started this activity and extract the List
         if(vocCardList ==null) {
             vocCardList = getIntent().getParcelableArrayListExtra("VocList");
         }
-        //Get the Data from the VocCards
-        final VocCard card4count = vocCardList.get(0);
-        int count = card4count.getCount();
-        if(count >= vocCardList.size()){
-            // TODO:
-             //Create new intent for EndPracticeActivity
+        mCardBackLayout = findViewById(R.id.card_back_frame);
+        mCardFrontLayout = findViewById(R.id.card_front_frame);
 
-            //makeshift statement
-            count--;
+        createRandIncline();
+        loadAnimations();
+        changeCameraDistance();
+        makeBackCardInvisible();
 
-        }else{
-           card4count.increaseCount();
-        }
-        final VocCard card = vocCardList.get(count);
-        String test = card.getVocForeign();
-
-        //Make a TextView display the transfered String
-        TextView textView = findViewById(R.id.word_foreign);
-        textView.setText(test);
+        //correctContinuePractice(); gehört zu einem neuen onClick
+        //wrongContinuePractice();  dito
+        applyStringsToCard();
 
         testVocCard();
 
@@ -54,16 +64,92 @@ public class PracticeActivity extends AppCompatActivity {
                 //intent.putParcelableArrayListExtra("voc1",(ArrayList)vocCardList);
                 getApplicationContext().startActivity(intent);
             }
-        }); /**/
+        });
 
+    }
+
+    private void makeBackCardInvisible() {
+        mCardBackLayout.setVisibility(View.INVISIBLE);
+        isBackVisible = false;
+    }
+
+    private void changeCameraDistance() {
+        int distance = 5000; // 5000 looks nice
+        float scale = getResources().getDisplayMetrics().density * distance;
+        mCardFrontLayout.setCameraDistance(scale);
+        mCardBackLayout.setCameraDistance(scale);
+    }
+
+    private void loadAnimations() {
+        mSetLeftOut = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.ani_out_left);
+        mSetRightIn = (AnimatorSet) AnimatorInflater.loadAnimator(this, R. animator.ani_in_right);
+        mSetRightOut = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.ani_out_right);
+        mSetLeftIn = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.ani_in_left);
+    }
+
+    private void createRandIncline() {
+        Random rand = new Random();
+        angle = rand.nextInt(31) - rand.nextInt(31); // creates angle between -30° and +30°
+        mCardBackLayout.setRotation(angle);
+        mCardFrontLayout.setRotation(-angle);
+    }
+
+    public void wrongContinuePractice(View view) {
+        //TODO: Switch VocCards within the ArrayList
+
+        Intent intent = new Intent(PracticeActivity.this, PracticeActivity.class);
+        getApplicationContext().startActivity(intent);
+    }
+
+    public void correctContinuePractice(View view) {
+        VocCard card4count = vocCardList.get(0);
+        count = card4count.getCount();
+        if(count >= vocCardList.size()){
+            // TODO:
+            //Create new intent for EndPracticeActivity
+
+            //makeshift statement
+            count--;
+
+        }else{
+            card4count.increaseCount();
+        }
+        Intent intent = new Intent(PracticeActivity.this, PracticeActivity.class);
+        getApplicationContext().startActivity(intent);
+    }
+
+    private void applyStringsToCard() {
+        VocCard card = vocCardList.get(count);
+        String foreignVoc = card.getVocForeign();
+        String nativeVoc = card.getVocNative();
+
+        //Make a TextView display the transfered String
+        TextView textViewFront = findViewById(R.id.word_foreign);
+        textViewFront.setText(foreignVoc);
+        TextView textViewBack = findViewById(R.id.word_native);
+        textViewBack.setText(nativeVoc);
     }
 
     private void testVocCard() {
 
     }
 
-
-
-    public void flipCurrentCard(View view) {
+    public void flipCard(View view) {
+        if(!isBackVisible) {
+            mCardBackLayout.setVisibility(View.VISIBLE);
+        }
+        if (!isBackRevealed) {
+            mSetLeftOut.setTarget(mCardFrontLayout);
+            mSetRightIn.setTarget(mCardBackLayout);
+            mSetLeftOut.start();
+            mSetRightIn.start();
+            isBackRevealed = true;
+        }else {
+            mSetRightOut.setTarget(mCardBackLayout);
+            mSetLeftIn.setTarget(mCardFrontLayout);
+            mSetRightOut.start();
+            mSetLeftIn.start();
+            isBackRevealed = false;
+        }
     }
 }
--- app/src/main/java/com/a2mar/smartcards/animations/FlipCard.java
+++ app/src/main/java/com/a2mar/smartcards/animations/FlipCard.java
@@ -1,9 +1,18 @@
 package com.a2mar.smartcards.animations;
 
+import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.FrameLayout;
 
-public class FlipCard {
+import com.a2mar.smartcards.R;
+
+import java.util.Random;
+
+public class FlipCard extends Activity {
 
     private AnimatorSet mSetLeftOut;
     private AnimatorSet mSetRightIn;
@@ -16,12 +25,51 @@ public class FlipCard {
     private int angle;
 
 
-    public FlipCard(){
-        findViews();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    public FlipCard(View front, View back){
+        //findViews();
+        mCardFrontLayout = front;
+        mCardBackLayout = back;
+        createRandIncline();
+        loadAnimations();
+        changeCameraDistance();
+        //makeBackCardInvisible();
+    }
+
+    private void makeBackCardInvisible() {
+        mCardBackLayout.setVisibility(View.INVISIBLE);
+        isBackVisible = false;
+    }
+
+    private void changeCameraDistance() {
+        int distance = 5000; // 5000 looks nice
+        float scale = getResources().getDisplayMetrics().density * distance;
+        mCardFrontLayout.setCameraDistance(scale);
+        mCardBackLayout.setCameraDistance(scale);
+    }
+
+    private void loadAnimations() {
+        mSetLeftOut = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.ani_out_left);
+        mSetRightIn = (AnimatorSet) AnimatorInflater.loadAnimator(this, R. animator.ani_in_right);
+        mSetRightOut = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.ani_out_right);
+        mSetLeftIn = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.ani_in_left);
+    }
+
+    private void createRandIncline() {
+        Random rand = new Random();
+        angle = rand.nextInt(31) - rand.nextInt(31); // creates angle between -30° and +30°
+        mCardBackLayout.setRotation(angle);
+        mCardFrontLayout.setRotation(angle);
     }
 
     private void findViews() {
-
+        mCardFrontLayout = findViewById(R.id.card_front_frame);
+        mCardBackLayout = findViewById(R.id.card_back_frame);
     }
 
     public void flipCard(View view) {
